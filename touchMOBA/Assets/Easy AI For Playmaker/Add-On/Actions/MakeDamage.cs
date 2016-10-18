9f18a3bd8265a4a193245c7a9501830a0a4c7f9b
lukeshark <luke@cloudyshark.com>
2016-10-18 16:23:29 -0500

    start on new ai

---
+++ touchMOBA/Assets/Easy AI For Playmaker/Add-On/Actions/MakeDamage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("Agents AI")]
+	public class MakeDamage : FsmStateAction {
+
+		public FsmGameObject Target;
+		public FsmFloat Damage;
+
+
+		public override void OnEnter() {
+			Target.Value.SendMessage("Damage", Damage.Value, SendMessageOptions.DontRequireReceiver);
+			Finish ();
+		}
+
+	
+}
+
+}
---
+++ touchMOBA/Assets/Easy AI For Playmaker/Add-On/Actions/TakeDamage.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TakeDamage : MonoBehaviour {
+
+
+	private PlayMakerFSM fsm;
+	public string NameOfVariableThatReceiveTheDamage;
+	public string NameOfFsm;
+	public string NameOfEventThatRunOnReceiveDamage;
+
+	void Awake() {
+
+		fsm = GetComponent<PlayMakerFSM> ();
+	}
+
+
+	public virtual void Damage(float damage)
+	{
+
+		var tempFSM = PlayMakerFSM.FindFsmOnGameObject (this.gameObject,NameOfFsm);
+			
+		tempFSM.FsmVariables.FindFsmFloat (NameOfVariableThatReceiveTheDamage).Value = damage;
+		tempFSM.SendEvent (NameOfEventThatRunOnReceiveDamage);
+	}
+}
+
+
+
