e4954a3c6e52a8cbc194a5ef989378f90c0f50e3
lukeshark <luke@cloudyshark.com>
2016-10-24 13:21:47 -0500

    various tweaks, loading bar

---
+++ touchMOBA/Assets/Scripts/FrameRateManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+ using System.Collections;
+ 
+ public class FrameRateManager : MonoBehaviour {
+	 
+	 
+	 void Awake () {
+		 QualitySettings.vSyncCount = 2;  // VSync must be disabled
+		 Application.targetFrameRate = 30;
+	 }
+ }
