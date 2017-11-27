57bbad57028e7bb75fc6f8ada8a06f63a47964ce
lukeshark <luke@cloudyshark.com>
2017-11-26 19:36:50 -0600

    new stuff

---
+++ DemonsDungeonPrototype/Assets/Art/Models/True_Horror_Creatures/Horror_creature3_v1.0/THC3_readme.txt
@@ -0,0 +1,20 @@
+NighMare Creature #3 readme.
+
+Controls:
+
+To change the combat and normal mode press 1 or 2
+(most actions available in combat mode)
+2 - turn on runing mode  ( to off press 1 );
+
+moving - directional arrows
+
+howl - x
+attack1 - mouse 1
+attack2 - mouse 2
+attack3 - mose 3
+hit - u
+die1 - i
+die2 - o
+
+I hope you will enjoy =)
+Kind regards, Grigoriy Arx
---
+++ DemonsDungeonPrototype/Assets/UFPS/Base/Scripts/Gameplay/Combat/DamageHandler_Monster.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHandler_Monster : vp_DamageHandler {
+
+    public Animator _animator;
+
+    public GameObject Mesh;
+    public Transform DeathFX;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        //_animator = GetComponent<Animator>();
+    }
+
+    public override void Damage(vp_DamageInfo damageInfo)
+    {
+        base.Damage(damageInfo);
+
+        _animator.SetTrigger("Hit");
+    }
+
+
+    public override void Die()
+    {
+        if (!enabled || !vp_Utility.IsActive(gameObject))
+            return;
+
+        if (m_Audio != null)
+        {
+            m_Audio.pitch = Time.timeScale;
+            m_Audio.PlayOneShot(DeathSound);
+        }
+
+        _animator.SetBool("Die", true);
+
+        foreach (GameObject o in DeathSpawnObjects)
+        {
+            if (o != null)
+            {
+                GameObject g = (GameObject)vp_Utility.Instantiate(o, Transform.position, Transform.rotation);
+                if ((Source != null) && (g != null))
+                    vp_TargetEvent<Transform>.Send(g.transform, "SetSource", OriginalSource);
+            }
+        }
+
+       
+
+    }
+
+    public void DestroyObjects()
+    {
+       // vp_Utility.Destroy(gameObject);
+       Destroy(Mesh);
+        DeathFX.parent = null;
+       //transform.DetachChildren();
+       //Destroy(this.gameObject);
+        
+        
+
+        // Destroy(GetComponent<Rigidbody>());
+        // Destroy(GetComponent<Animator>());
+
+
+
+    }
+
+}
